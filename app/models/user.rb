ea3410df24438ca35f53e8f400112d323a3120b4
smatsuo <s.pincoda@i.gmobb.jp>
2016-08-06 12:28:52 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,38 @@ class User < ActiveRecord::Base
     
     has_secure_password
     has_many :microposts
+    
+    has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
+    
+    has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+    has_many :follower_users, through: :follower_relationships, source: :follower
+  
+#   following_relationshipsのforeign_keyのfollower_idにuserのidが入るので、
+#   user.following_relationshipsによって、userがフォローしている場合のrelationshipの集まりを取得することができます。
+# following_usersでは、has_many 〜 throughという文を使っています。throughには、following_relationshipsが指定されていて、
+# 上の図のように、following_relationshipsを経由してフォローしているユーザーの集まりを取得することを意味しています。
+# userがフォローしている人は、following_relationshipsのfollowed_idに一致するユーザーになるので、
+# sourceとしてfollowedを指定しています。
+
+  # 他のユーザーをフォローする
+  def follow(other_user)
+    following_relationships.find_or_create_by(followed_id: other_user.id)
+  end
+
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationship = following_relationships.find_by(followed_id: other_user.id)
+    following_relationship.destroy if following_relationship
+  end
+
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
+  
 end
